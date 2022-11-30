0fe9a64991581624d73f1436559c3ccb328a70fa
HeitorPupim <heitorpupimtoledo@gmail.com>
2022-11-30 00:06:51 -0300

    alteração Q10.sql

--- BasesDeDados/Pratica2/Q10.sql
+++ BasesDeDados/Pratica2/Q10.sql
@@ -1,12 +1,12 @@
--- Q10: (X) Selecionar times que nunca jogaram em São Carlos e em Belo Horizonte.	
-
-      	SELECT * FROM partida; -- carrega LOCAL
-      	--precisamos verificar os times que jogaram em São Carlos e BH
-      	SELECT p.TIME1, p.TIME2 FROM PARTIDA p 
+-- Q10: (X) Selecionar times que nunca jogaram em São Carlos e em Belo Horizonte
+	--select times que jogaram em sanca e bh
+      	SELECT * FROM PARTIDA p 
       		WHERE p."LOCAL" IN ('SAO CARLOS', 'BELO HORIZONTE');
-						
-  		-- selecionando todas as partidas em que os times não jogaram em sanca ou bh:
-      	SELECT * 
-  			FROM PARTIDA p 
-  				WHERE (p.TIME1, p.TIME2) NOT IN (SELECT p.TIME1, p.TIME2 FROM PARTIDA p 
-      										WHERE p."LOCAL" IN ('SAO CARLOS', 'BELO HORIZONTE'));		
+      	
+      	
+  	-- selecionar todos menos os times que estão na query acima:
+      	SELECT * FROM "TIME" t 
+      		WHERE NOT EXISTS 
+      			(SELECT * FROM PARTIDA p 
+ 		     		WHERE p."LOCAL" IN ('SAO CARLOS', 'BELO HORIZONTE')
+ 		     			AND (p.TIME1 = t.NOME OR p.TIME2 = t.NOME));
