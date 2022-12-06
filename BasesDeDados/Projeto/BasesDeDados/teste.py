c10f0f4f14aca13dcb467a162105aa738ceed17c
HeitorPupim <heitorpupimtoledo@gmail.com>
2022-12-06 20:37:59 -0300

    codigo teste deletado

--- BasesDeDados/Projeto/BasesDeDados/teste.py
+++
@@ -1,7 +0,0 @@
-def validateString(string):
-   lista = [";", "'", "--", "/*", "xp_"]
-   if any(x in string for x in lista):
-      print("Erro: String inválida!!!! Tente novamente")
-
-
-inp_string = validateString(input("Digite uma string: "))
