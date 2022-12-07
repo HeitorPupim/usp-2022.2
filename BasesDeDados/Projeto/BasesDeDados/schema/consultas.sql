1f84bdc8f994e4ea53cafc81c825a92443b9e56a
Rafael Jun Teramae Dantas <87828050+RafaelJunDantas@users.noreply.github.com>
2022-12-06 22:21:02 -0300

    add consulta 6

--- BasesDeDados/Projeto/BasesDeDados/schema/consultas.sql
+++ BasesDeDados/Projeto/BasesDeDados/schema/consultas.sql
@@ -39,3 +39,15 @@ select pc.cpf, pc.nome, sum(r.quantidade) as total_refeicoes, r.organizacao
     right join pessoa_carente pc on pc.cpf = r.pessoa_carente 
     group by pc.cpf, pc.nome, r.organizacao
     order by pc.cpf, r.organizacao;
+
+--6) busca a media de doacao em dinheiro de um doador em todas as organizacoes que doou
+select o.cnpj, o.nome, avg(di.valor)
+	from organizacao o join doacao d on o.cnpj = d.organizacao 
+   		join dinheiro di on d.doador = di.doador and d.dia_hora = di.dia_hora 
+   	group by o.cnpj, o.nome
+   	except 
+   	select o2.cnpj, o2.nome, sum(di2.valor)
+   		from organizacao o2 join doacao d2 on o2.cnpj = d2.organizacao 
+   			join dinheiro di2 on d2.doador = di2.doador and d2.dia_hora = di2.dia_hora
+   		where d2.doador <> 11		--doador que quer saber a media de dinhieiro doado por organizacao
+   		group by o2.cnpj, o2.nome;
